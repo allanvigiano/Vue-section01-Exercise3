ce4820ade29542557acf28c53d31235ec0ef12ae
Allan de Almeida Vigiano <allanvigiano@gmail.com>
2016-11-24 15:02:19 -0200

    Solution commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        value: 0,
+        result: '',
+    },
+    computed: {
+        result: function () {
+            return this.value != 37 ? 'not there yet' : 'done';
+        }
+    },
+    watch: {
+        result: function () {
+            var vm = this;
+            setTimeout(function () {
+                vm.value = 0;
+            }, 5000);
+        }
+    },
+    methods: {
+
+    }
+});
